c7bc7657969b42ea72b4d1ae04fd19970e02a424
xpcnacker <stefanmueller123222@gmail.com>
2026-06-01 17:32:46 +0200

    Add files via upload

---
+++ include/keygen_interface.h
@@ -0,0 +1,15 @@
+#ifndef KEYGEN_IFACE_H
+#define KEYGEN_IFACE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Diese Funktion implementieren wir in main.cpp
+void generate_key(char* buffer);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
